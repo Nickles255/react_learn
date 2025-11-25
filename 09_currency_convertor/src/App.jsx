aaa1ef82fe6c015fbcfee1d425e1896f072df97a
clin94118 <clin@ccsf.edu>
2025-11-25 05:19:35 -0800

    docs: initial commit

---
+++ 09_currency_convertor/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    const [inputAmt, setInputAmt] = useState("");
+    const [inputCurrency, setInputCurrency] = useState("");
+    const [outputCurrency, setOutputCurrency] = useState("");
+
+  return (
+    <div>
+      <input type="text" />
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT</p>
+    </div>
+  )
+}
+
+export default App
