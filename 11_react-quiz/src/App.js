12ad791ce4d84712cb643abbacea7e00530ae0cb
clin94118 <clin@ccsf.edu>
2025-12-09 08:33:56 -0800

    feat(11_react-quiz): integrate status-based rendering
    - Updated `App` to conditionally render `Loader`, `Error`, or `StartScreen` based on `status`.
    - Introduced `StartScreen` component to display introductory quiz information.

--- 11_react-quiz/src/App.js
+++ 11_react-quiz/src/App.js
@@ -1,6 +1,9 @@
 import Header from "./Header";
 import {useEffect, useReducer} from "react";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
     questions: [],
@@ -27,7 +30,9 @@ function reducer(state, action) {
 }
 
 export default function App() {
-    const [state, dispatch] = useReducer(reducer, initialState)
+    const [{questions, status}, dispatch] = useReducer(reducer, initialState)
+
+    const numQuestions = questions.length;
 
     useEffect(() => {
         fetch('http://localhost:8000/questions')
@@ -40,8 +45,9 @@ export default function App() {
         <div className="app">
             <Header/>
             <Main>
-                <p>Questions</p>
-                <p>1/15</p>
+                {status === 'loading' && <Loader />}
+                {status === 'error' && <Error />}
+                {status === 'ready' && <StartScreen numQuestions={numQuestions}/>}
             </Main>
         </div>
     );
