cb56deabf609e423131d46f988c14246f0471974
clin94118 <clin@ccsf.edu>
2025-12-09 09:55:19 -0800

    feat(11_react-quiz): implement question interactions and navigation
    - Added `Question`, `Options`, and `NextButton` components to handle quiz interactions.
    - Enhanced `App` state with support for tracking answers, points, and question index.
    - Updated reducer logic with `newAnswer` and `nextQuestion` actions for dynamic state management.
    - Improved conditional rendering to support active quiz flow.

--- 11_react-quiz/src/App.js
+++ 11_react-quiz/src/App.js
@@ -5,11 +5,15 @@ import Loader from "./components/Loader";
 import Error from "./components/Error";
 import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
+import NextButton from "./components/NextButton";
 
 const initialState = {
     questions: [],
     // 'loading', 'error', 'ready', 'active', 'finished'
     status: 'loading',
+    index: 0,
+    answer: null,
+    points: 0
 }
 
 function reducer(state, action) {
@@ -18,20 +22,37 @@ function reducer(state, action) {
             return {
                 ...state,
                 questions: action.payload,
-                status: 'ready'
+                status: 'ready',
             };
         case 'dataFailed':
             return {
                 ...state,
-                status: 'error'
+                status: 'error',
+            };
+        case 'start':
+            return {
+                ...state,
+                status: 'active',
+            };
+        case 'newAnswer':
+            const question = state.questions.at(state.index);
+
+            return {
+                ...state,
+                answer: action.payload,
+                points: action.payload === question.correctOption
+                    ? state.points + question.points
+                    : state.points,
             }
+        case 'nextQuestion':
+            return {...state, index: state.index + 1, answer: null};
         default:
             throw new Error('Unknown action');
     }
 }
 
 export default function App() {
-    const [{questions, status}, dispatch] = useReducer(reducer, initialState)
+    const [{questions, status, index, answer}, dispatch] = useReducer(reducer, initialState)
 
     const numQuestions = questions.length;
 
@@ -48,11 +69,25 @@ export default function App() {
             <Main>
                 {status === 'loading' && <Loader />}
                 {status === 'error' && <Error />}
-                {status === 'ready' && <StartScreen numQuestions={numQuestions}/>}
-                {status === 'ready' && <StartScreen
-                    numQuestions={numQuestions}
-                    dispatch={dispatch}
-                />}
+                {status === 'ready' &&
+                    <StartScreen
+                        numQuestions={numQuestions}
+                        dispatch={dispatch}
+                    />
+                }
+                {status === 'active' &&
+                    <>
+                        <Question
+                            question={questions[index]}
+                            dispatch={dispatch}
+                            answer={answer}
+                        />
+                        <NextButton
+                            dispatch={dispatch}
+                            answer={answer}
+                        />
+                    </>
+                }
             </Main>
         </div>
     );
