af1cc0e797ced1ce6acd2d1abecb4a5ec1e62f91
clin94118 <clin@ccsf.edu>
2025-12-09 07:56:22 -0800

    feat(11_react-quiz): integrate `useReducer` for managing questions state and API error handling
    - Added `useReducer` to manage application state with `questions` and `status` fields.
    - Implemented `dataReceived` and `dataFailed` actions for handling API responses.
    - Updated `useEffect` to dispatch actions based on API fetch results.

--- 11_react-quiz/src/App.js
+++ 11_react-quiz/src/App.js
@@ -2,23 +2,48 @@ import Header from "./Header";
 import {useEffect, useReducer} from "react";
 import Main from "./Main";
 
+const initialState = {
+    questions: [],
+    // 'loading', 'error', 'ready', 'active', 'finished'
+    status: 'loading',
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'dataReceived':
+            return {
+                ...state,
+                questions: action.payload,
+                status: 'ready'
+            };
+        case 'dataFailed':
+            return {
+                ...state,
+                status: 'error'
+            }
+        default:
+            throw new Error('Unknown action');
+    }
+}
+
 export default function App() {
+    const [state, dispatch] = useReducer(reducer, initialState)
 
     useEffect(() => {
         fetch('http://localhost:8000/questions')
             .then((res) => res.json())
-            .then((data) => console.log(data))
-            .catch((err) => console.error("Error"));
+            .then((data) => dispatch({type: 'dataReceived', payload: data}))
+            .catch((err) => dispatch({type: 'dataFailed'}));
     }, []);
 
     return (
-      <div className="app">
-          <Header />
-          <Main>
-              <p>Questions</p>
-              <p>1/15</p>
-          </Main>
-      </div>
+        <div className="app">
+            <Header/>
+            <Main>
+                <p>Questions</p>
+                <p>1/15</p>
+            </Main>
+        </div>
     );
 
 }
