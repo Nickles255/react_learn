ba09ce1ebb287886a0664f92327b9a45f49d4f70
clin94118 <clin@ccsf.edu>
2025-12-15 19:27:41 -0800

    feat(13_worldwise): add routing and initial page components
    - Added React Router to manage client-side routing with `BrowserRouter` and `Routes`.
    - Created `Homepage`, `Pricing`, `Product`, and `PageNotFound` page components.
    - Implemented `PageNav` component for navigation links between pages.
    - Updated `App.jsx` to include routing logic for the application.

--- 13_worldwise/src/App.jsx
+++ 13_worldwise/src/App.jsx
@@ -1,9 +1,24 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Product from "./pages/product.jsx";
+import Pricing from "./pages/pricing.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+
 function App() {
     return (
         <div>
-            WorldWise
+            <h1>Hello Router!</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Homepage/>}/>
+                    <Route path="product" element={<Product/>}/>
+                    <Route path="pricing" element={<Pricing/>}/>
+                    <Route path="*" element={<PageNotFound/>}/>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
+
 
 export default App;
