3996a03cb657784c58566d96276ff4234210aff4
clin94118 <clin@ccsf.edu>
2025-12-17 08:17:37 -0800

    feat(13_worldwise): add nested routing in App using Outlet components
    - Modified routing in `App.jsx` to support nested routes under `/app` (e.g., `cities`, `countries`, `form`).
    - Added `Outlet` component to `Sidebar` for rendering nested routes.
    - Updated `AppNav` to include navigable links for `Cities` and `Countries`.

--- 13_worldwise/src/App.jsx
+++ 13_worldwise/src/App.jsx
@@ -5,21 +5,27 @@ import Pricing from "./pages/pricing.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
 import Login from "./pages/Login.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
+
 // import Login from "./pages/Login.jsx";
 
 function App() {
     return (<div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Homepage />}/>
-                    <Route path="product" element={<Product />}/>
-                    <Route path="pricing" element={<Pricing />}/>
-                    <Route path="app" element={<AppLayout />}/>
-                    <Route path="login" element={<Login />}/>
-                    <Route path="*" element={<PageNotFound />}/>
-                </Routes>
-            </BrowserRouter>
-        </div>);
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Homepage/>} />
+                <Route path="product" element={<Product/>} />
+                <Route path="pricing" element={<Pricing/>} />
+                <Route path="app" element={<AppLayout/>}>
+                    <Route index element={<p>List of Cities</p>} />
+                    <Route path="cities" element={<p>List of Cities</p>} />
+                    <Route path="countries" element={<p>Countries</p>} />
+                    <Route path="form" element={<p>Form</p>} />
+                </Route>
+                <Route path="login" element={<Login/>} />
+                <Route path="*" element={<PageNotFound/>} />
+            </Routes>
+        </BrowserRouter>
+    </div>);
 }
 
 
--- 13_worldwise/src/components/Sidebar.jsx
+++ 13_worldwise/src/components/Sidebar.jsx
@@ -2,12 +2,15 @@ import styles from "./Sidebar.module.css";
 import Logo from "./Logo.jsx";
 import AppNav from "./AppNav.jsx";
 import Footer from "./Footer.jsx";
+import {Outlet} from "react-router-dom";
 
 function Sidebar() {
     return (
         <div className={styles.sidebar}>
             <Logo />
             <AppNav />
+
+            <Outlet />
 
             <Footer />
         </div>
