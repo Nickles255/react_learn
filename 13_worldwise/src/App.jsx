4b551eae7ecc4640e9200ec51ba0c6a145f02dae
clin94118 <clin@ccsf.edu>
2025-12-18 05:53:45 -0800

    feat(13_worldwise): fetch and display cities with new CityList and CityItem components
    - Added `CityList` and `CityItem` components to display a list of cities.
    - Integrated city data fetching in `App.jsx` using `useEffect` and `useState` hooks.
    - Displayed cities dynamically on `/app` and `/app/cities` routes.
    - Styled `CityList` and `CityItem` with new CSS modules.

--- 13_worldwise/src/App.jsx
+++ 13_worldwise/src/App.jsx
@@ -3,21 +3,43 @@ import Homepage from "./pages/Homepage.jsx";
 import Product from "./pages/product.jsx";
 import Pricing from "./pages/pricing.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
+import CityList from "./components/CityList.jsx";
 import Login from "./pages/Login.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
+import {useEffect, useState} from "react";
 
+const BASE_URL = "http://localhost:8000"
 // import Login from "./pages/Login.jsx";
 
 function App() {
+    const [cities, setCities] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(function(){
+        async function fetchCities(){
+            try {
+                setIsLoading(true);
+                const res = await fetch(`${BASE_URL}/cities`);
+                const data = await res.json();
+                setCities(data);
+            } catch {
+                alert('There was an error loading data...')
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchCities();
+    }, [])
+
     return (<div>
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Homepage/>} />
-                <Route path="product" element={<Product/>} />
-                <Route path="pricing" element={<Pricing/>} />
-                <Route path="app" element={<AppLayout/>}>
-                    <Route index element={<p>List of Cities</p>} />
-                    <Route path="cities" element={<p>List of Cities</p>} />
+                <Route path="/" element={<Homepage />} />
+                <Route path="product" element={<Product />} />
+                <Route path="pricing" element={<Pricing />} />
+                <Route path="app" element={<AppLayout />}>
+                    <Route index element={<CityList cities={cities} isLoading={isLoading}/>} />
+                    <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
                     <Route path="countries" element={<p>Countries</p>} />
                     <Route path="form" element={<p>Form</p>} />
                 </Route>
