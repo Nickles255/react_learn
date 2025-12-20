8d1a00cf406a1fdc50c3c122b661e441ed962ab7
clin94118 <clin@ccsf.edu>
2025-12-20 10:11:33 -0800

    feat(13_worldwise): add City component and dynamic city details route
    - Created `City` component to display city details dynamically.
    - Updated routing in `App.jsx` to handle `/app/cities/:id` route using `useParams`.
    - Enhanced `CityItem` to link to the specific city details page.

--- 13_worldwise/src/App.jsx
+++ 13_worldwise/src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import {useEffect, useState} from "react";
 import CountryList from "./components/CountryList.jsx";
+import City from "./components/City.jsx";
 
 const BASE_URL = "http://localhost:8000"
 // import Login from "./pages/Login.jsx";
@@ -43,6 +44,9 @@ function App() {
                            element={<CityList cities={cities} isLoading={isLoading}/>} />
                     <Route path="cities"
                            element={<CityList cities={cities} isLoading={isLoading}/>}
+                    />
+                    <Route path="cities/:id"
+                        element={<City />}
                     />
                     <Route path="countries"
                            element={<CountryList cities={cities} isLoading={isLoading}/>}
