ef68b5c1840b31d0d6fc6ad411131af6d8f84f78
clin94118 <clin@ccsf.edu>
2026-01-05 09:43:52 -0800

    feat(atomic-blog): split `App.js` into modular components
    - Extracted `FormAddPost`, `Posts`, `Main`, `List`, and `createRandomPost` into separate files for better organization and reusability.
    - Updated `App.js` to import and incorporate the new component structure.

--- 12-atomic-blog/starter/src/App.js
+++ 12-atomic-blog/starter/src/App.js
@@ -3,13 +3,11 @@ import {faker} from "@faker-js/faker";
 import Button from "./Components/Button";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import Posts from "./Components/Posts";
+import FormAddPost from "./Components/FormAddPost";
+import Main from "./Components/Main";
+import createRandomPost from "./Components/createRandomPost";
 
-function createRandomPost() {
-    return {
-        title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
-        body: faker.hacker.phrase(),
-    };
-}
 
 // 1) create a context
 export const PostContext = createContext();
@@ -67,75 +65,12 @@ function App() {
                 <Button className="btn-fake-dark-mode">
                     {isFakeDark ? "☀️" : "🌙"}
                 < /Button>
-
-                />
                 <Header/>
                 <Main />
                 <Archive />
                 <Footer/>
             </section>
         </PostContext.Provider>
-    );
-}
-
-function Main() {
-    return (
-        <main>
-            <FormAddPost />
-            <Posts />
-        </main>
-    );
-}
-
-function Posts() {
-    return (
-        <section>
-            <List />
-        </section>
-    );
-}
-
-function FormAddPost() {
-    const {onAddPost} = useContext(PostContext);
-    const [title, setTitle] = useState("");
-    const [body, setBody] = useState("");
-
-    const handleSubmit = function (e) {
-        e.preventDefault();
-        if (!body || !title) return;
-        onAddPost({title, body});
-        setTitle("");
-        setBody("");
-    };
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <input
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                placeholder="Post title"
-            />
-            <textarea
-                value={body}
-                onChange={(e) => setBody(e.target.value)}
-                placeholder="Post body"
-            />
-            <button>Add post</button>
-        </form>
-    );
-}
-
-function List() {
-    const {posts} = useContext(PostContext);
-    return (
-        <ul>
-            {posts.map((post, i) => (
-                <li key={i}>
-                    <h3>{post.title}</h3>
-                    <p>{post.body}</p>
-                </li>
-            ))}
-        </ul>
     );
 }
 
