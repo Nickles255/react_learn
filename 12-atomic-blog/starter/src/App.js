644737ed1a7e2572508181084bc35c11042127a2
clin94118 <clin@ccsf.edu>
2026-01-05 09:49:02 -0800

    feat(atomic-blog): extract `Archive` into a separate component
    - Moved `Archive` component from `App.js` to a dedicated `Archive.jsx` file for improved modularity.
    - Updated `App.js` to import and utilize the new `Archive` component.

--- 12-atomic-blog/starter/src/App.js
+++ 12-atomic-blog/starter/src/App.js
@@ -1,12 +1,10 @@
-import {createContext, useContext, useEffect, useState} from "react";
-import {faker} from "@faker-js/faker";
-import Button from "./Components/Button";
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
-import Posts from "./Components/Posts";
-import FormAddPost from "./Components/FormAddPost";
-import Main from "./Components/Main";
+import {createContext, useEffect, useState} from "react";
 import createRandomPost from "./Components/createRandomPost";
+import Button from "./Components/Button";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Archive from "./Components/Archive";
+import Footer from "./Components/Footer";
 
 
 // 1) create a context
@@ -73,38 +71,4 @@ function App() {
         </PostContext.Provider>
     );
 }
-
-function Archive() {
-    // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
-    const [posts] = useState(() =>
-        // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
-        Array.from({length: 10000}, () => createRandomPost())
-    );
-
-    const {onAddPost} = useContext(PostContext);
-    const [showArchive, setShowArchive] = useState(false);
-
-    return (
-        <aside>
-            <h2>Post archive</h2>
-            <button onClick={() => setShowArchive((s) => !s)}>
-                {showArchive ? "Hide archive posts" : "Show archive posts"}
-            </button>
-
-            {showArchive && (
-                <ul>
-                    {posts.map((post, i) => (
-                        <li key={i}>
-                            <p>
-                                <strong>{post.title}:</strong> {post.body}
-                            </p>
-                            <button onClick={() => onAddPost(post)}>Add as new post</button>
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </aside>
-    );
-}
-
 export default App;
