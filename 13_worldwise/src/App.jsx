72f6daf9cd0d88b92d4b4b01a6ac997bc8f49166
clin94118 <clin@ccsf.edu>
2026-01-22 13:01:37 -0800

    refactor(13_worldwise): implement lazy loading with React Suspense for routes
    - Replaced static imports for pages with lazy-loaded imports.
    - Wrapped `Routes` with `Suspense` for handling fallback loading state.

--- 13_worldwise/src/App.jsx
+++ 13_worldwise/src/App.jsx
@@ -1,17 +1,29 @@
+import {lazy, Suspense} from "react";
 import {BrowserRouter, Navigate, Routes, Route} from "react-router-dom";
-import Homepage from "./pages/Homepage.jsx";
-import Product from "./pages/product.jsx";
-import Pricing from "./pages/pricing.jsx";
-import AppLayout from "./pages/AppLayout.jsx";
-import CityList from "./components/CityList.jsx";
-import Login from "./pages/Login.jsx";
-import PageNotFound from "./pages/PageNotFound.jsx";
-import CountryList from "./components/CountryList.jsx";
-import City from "./components/City.jsx";
-import Form from "./components/Form.jsx";
+
 import {CitiesProvider} from "./contexts/CitiesContext.jsx";
 import {AuthProvider} from "./contexts/FakeAuthContext.jsx";
 import ProtectedRoute from "./pages/ProtectedRoute.jsx";
+
+import CityList from "./components/CityList.jsx";
+import CountryList from "./components/CountryList.jsx";
+import City from "./components/City.jsx";
+import Form from "./components/Form.jsx";
+import SpinnerFullPage from "./components/SpinnerFullPage.jsx";
+
+// import Homepage from "./pages/Homepage.jsx";
+// import Product from "./pages/product.jsx";
+// import Pricing from "./pages/pricing.jsx";
+// import Login from "./pages/Login.jsx";
+// import AppLayout from "./pages/AppLayout.jsx";
+// import PageNotFound from "./pages/PageNotFound.jsx";
+
+const Homepage = lazy(() => import("./pages/Homepage.jsx"));
+const Product = lazy(() => import("./pages/Product.jsx"));
+const Pricing = lazy(() => import("./pages/Pricing.jsx"));
+const Login = lazy(() => import("./pages/Login.jsx"));
+const AppLayout = lazy(() => import("./pages/AppLayout.jsx"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound.jsx"));
 
 
 // import Login from "./pages/Login.jsx";
@@ -22,27 +34,29 @@ function App() {
         <AuthProvider>
             <CitiesProvider>
                 <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Homepage/>}/>
-                        <Route path="product" element={<Product/>}/>
-                        <Route path="pricing" element={<Pricing/>}/>
-                        <Route
-                            path="app"
-                            element={
-                                <ProtectedRoute>
-                                    <AppLayout/>
-                                </ProtectedRoute>
-                            }
-                        >
-                            <Route index element={<Navigate replace to="cities"/>}/>
-                            <Route path="cities" element={<CityList/>}/>
-                            <Route path="cities/:id" element={<City/>}/>
-                            <Route path="countries" element={<CountryList/>}/>
-                            <Route path="form" element={<Form/>}/>
-                        </Route>
-                        <Route path="login" element={<Login/>}/>
-                        <Route path="*" element={<PageNotFound/>}/>
-                    </Routes>
+                    <Suspense fallback={<SpinnerFullPage/>}>
+                        <Routes>
+                            <Route path="/" element={<Homepage/>}/>
+                            <Route path="product" element={<Product/>}/>
+                            <Route path="pricing" element={<Pricing/>}/>
+                            <Route path="login" element={<Login/>}/>
+                            <Route
+                                path="app"
+                                element={
+                                    <ProtectedRoute>
+                                        <AppLayout/>
+                                    </ProtectedRoute>
+                                }
+                            >
+                                <Route index element={<Navigate replace to="cities"/>}/>
+                                <Route path="cities" element={<CityList/>}/>
+                                <Route path="cities/:id" element={<City/>}/>
+                                <Route path="countries" element={<CountryList/>}/>
+                                <Route path="form" element={<Form/>}/>
+                            </Route>
+                            <Route path="*" element={<PageNotFound/>}/>
+                        </Routes>
+                    </Suspense>
                 </BrowserRouter>
             </CitiesProvider>
         </AuthProvider>
